eab7096991a79aa85e7326db3d511b0172973955
ffidls <alinka.orehova@gmail.com>
2022-03-29 01:25:21 +0300

    s

--- bot_functions/menu.py
+++ bot_functions/menu.py
@@ -4,7 +4,7 @@ from telegram import ReplyKeyboardMarkup
 def menu(update, context):
     word_password = update.message.text
 
-    markup = ReplyKeyboardMarkup([['/adviсe', '/rating'], ['/site', '/shop']], one_time_keyboard=True)
+    markup = ReplyKeyboardMarkup([['/advice', '/rating'], ['/site', '/shop']], one_time_keyboard=True)
     update.message.reply_text(
         "Какая информация вам нужна?",
         reply_markup=markup
